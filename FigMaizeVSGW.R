b0cfcb4857c5645c76097b7009fe65dc237c0908
Kalicz Péter <kaliczp@gmail.com>
2024-02-12 23:29:49 +0100

    Barplot draft

--- FigMaizeVSGW.R
+++ FigMaizeVSGW.R
@@ -1,3 +1,20 @@
 library(readxl)
 GWmaizeraw <- as.data.frame(read_excel("WG3_abra-adatok_Peternek.xlsx", range = "abra3!B5:F24", col_names = FALSE))
 colnames(GWmaizeraw) <- c("Year", "GW", "Loam", "Clay","Sand")
+
+## Soil
+MaizeSoil <- t(GWmaizeraw[,c("Loam", "Clay", "Sand")])
+colnames(MaizeSoil) <- GWmaizeraw$Year
+
+## gw
+GWmaize <- t(GWmaizeraw[["GW"]])
+colnames(GWmaize) <- GWmaizeraw$Year
+
+par(mar = c(5.1,4.1,1.1,4.1), las = 2)
+barplot(MaizeSoil, beside = TRUE, ylim = c(0, 140), yaxt = "n")
+axis(4, at = seq(0,100, by = 20))
+grid(nx=NA, ny = NULL)
+par(new = TRUE)
+barplot(GWmaize, ylim = c(14,0),
+        xaxt = "n", yaxt = "n")
+axis(2, at = 0:7)
