3d028b78d0d4c069b328f840b286c2e060f4d403
Kalicz Péter <kaliczp@gmail.com>
2024-02-12 22:29:03 +0100

    Read GW sheet

---
+++ FigGW_1971_2010.R
@@ -0,0 +1,5 @@
+library(readxl)
+gwraw <- as.data.frame(read_excel("WG3_abra-adatok_Peternek.xlsx", range = "abra1!B5:E14614", col_names = FALSE))
+colnames(gwraw) <- c("Date", "gw2680", "gw2683", "gw2684")
+library(xts)
+gw.xts <- xts(gwraw[, -1], gwraw[,1])
