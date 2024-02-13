9bff256a0544646979d93a6f4a95ccd86a01cc94
Kalicz Péter <kaliczp@gmail.com>
2024-02-13 20:08:05 +0100

    First version of the maize figure

--- MaizeSim.R
+++ MaizeSim.R
@@ -1,3 +1,15 @@
 library(readxl)
 maizeraw <- as.data.frame(read_excel("WG3_abra-adatok_Peternek.xlsx", range = "abra2!B5:D24", col_names = FALSE))
 colnames(maizeraw) <- c("Year", "Country", "Sim")
+
+## data preparation
+Maizesim <- t(maizeraw[,2:3])
+colnames(Maizesim) <- maizeraw$Year
+
+maizecolors <- gwcolors[2:3] # dark and lighter from FigMaizeVSGW.R
+
+par(mar = c(3.1,4.1,0.6,4.1), las = 2)
+barplot(Maizesim, beside = TRUE, ylim = c(0,9), col = NA)
+grid(nx=NA, ny = NULL)
+barplot(Maizesim, beside = TRUE, ylim = c(0,9), col = maizecolors)
+mtext("Maize yield [t ha-1]", side = 2, line = 3, las = 3, at = 4)
