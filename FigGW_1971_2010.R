156a5f57e9def3c95d7d5c3cfa0aa3ea64768bc3
Kalicz Péter <kaliczp@gmail.com>
2024-02-13 22:37:00 +0100

    First draft of gw figure

--- FigGW_1971_2010.R
+++ FigGW_1971_2010.R
@@ -3,3 +3,9 @@ gwraw <- as.data.frame(read_excel("WG3_abra-adatok_Peternek.xlsx", range = "abra
 colnames(gwraw) <- c("Date", "gw2680", "gw2683", "gw2684")
 library(xts)
 gw.xts <- xts(gwraw[, -1], gwraw[,1])
+
+plot.zoo(gw.xts$gw2680, ylim = c(-6,0), xaxs = "i", type = "n",
+         xlab = "", ylab = "GWL [m below ground surface]")
+lines(as.zoo(gw.xts$gw2680))
+lines(as.zoo(gw.xts$gw2683))
+lines(as.zoo(gw.xts$gw2684))
