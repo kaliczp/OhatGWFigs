34a9cd1fe3353bda66879a3f2a556e9f1a056e44
Kalicz Péter <kaliczp@gmail.com>
2024-02-13 07:14:00 +0100

    pdf created with legend and box

--- FigMaizeVSGW.R
+++ FigMaizeVSGW.R
@@ -16,7 +16,8 @@ gwcolors <- c("#2c7bb6", # blueish
               "#ffffbf" # lightest
               )
 
-par(mar = c(5.1,4.1,1.1,4.1), las = 2)
+pdf("Fig4MaizeVSGW.pdf", width = 90 / 25.4, height = 60 / 25.4, pointsize = 7)
+par(mar = c(3.1,4.1,0.6,4.1), las = 2)
 barplot(MaizeSoil, beside = TRUE, ylim = c(0, 140), yaxt = "n", type = "n", col = NA)
 axis(4, at = seq(0,100, by = 20))
 grid(nx=NA, ny = NULL)
@@ -27,3 +28,6 @@ barplot(GWmaize, ylim = c(14,0),
         xaxt = "n", yaxt = "n", col = gwcolors[1])
 axis(2, at = seq(0,8,by = 2))
 mtext("GWL [m below surface]", side = 2, line = 3, at = 4, las = 3)
+legend(1.8, 5, legend = c("GWL", "loam", "clay", "sand"), fill = gwcolors,  bg = "white", cex = 0.8)
+box()
+dev.off()
