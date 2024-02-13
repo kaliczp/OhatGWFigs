5335da6af010088ffcbfcdab05489a8b181c7e02
Kalicz Péter <kaliczp@gmail.com>
2024-02-13 22:07:15 +0100

    Legend placed at coordinate

--- MaizeSim.R
+++ MaizeSim.R
@@ -9,12 +9,13 @@ colnames(Maizesim) <- maizeraw$Year
 maizecolors <- gwcolors[2:3] # dark and lighter from FigMaizeVSGW.R
 
 png("Fig3Maizeyield.png", width = 90, height = 60, units = "mm", pointsize = 7, res = 300)
+
 pdf("Fig3Maizeyield.pdf", width = 90 / 25.4, height = 60 / 25.4, pointsize = 7)
 par(mar = c(3.1,3.6,0.6,0.6), las = 2)
 barplot(matrix(NA, nrow = nrow(Maizesim), ncol = ncol(Maizesim)), beside = TRUE, ylim = c(0,9), col = NA)
 grid(nx=NA, ny = NULL)
 barplot(Maizesim, beside = TRUE, ylim = c(0,9), col = maizecolors, add = TRUE, border = NA)
 mtext(expression(paste("Maize yield [", t %.% ha^{-1}, "]")), side = 2, line = 2.3, las = 3, at = 4)
-legend("topleft", legend = c("Statistical county-level data", "Simulated for the loam profile"),
+legend(3, 8.9, legend = c("Statistical county-level data", "Simulated for the loam profile"),
        fill = maizecolors,  bg = "white", cex = 0.8)
 dev.off()
