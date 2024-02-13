6a1a43fa7a308d5a97b474cbaaa71b8db5ac0523
Kalicz Péter <kaliczp@gmail.com>
2024-02-13 20:36:14 +0100

    Plotted version with replaced legend

--- MaizeSim.R
+++ MaizeSim.R
@@ -8,10 +8,14 @@ colnames(Maizesim) <- maizeraw$Year
 
 maizecolors <- gwcolors[2:3] # dark and lighter from FigMaizeVSGW.R
 
-par(mar = c(3.1,4.6,0.6,4.1), las = 2)
+png("Fig3Maizeyield.png", width = 90, height = 60, units = "mm", pointsize = 7, res = 300)
+pdf("Fig3Maizeyield.pdf", width = 90 / 25.4, height = 60 / 25.4, pointsize = 7)
+par(mar = c(3.1,3.6,0.6,0.6), las = 2)
 barplot(Maizesim, beside = TRUE, ylim = c(0,9), col = NA)
 grid(nx=NA, ny = NULL)
 barplot(Maizesim, beside = TRUE, ylim = c(0,9), col = maizecolors, add = TRUE)
-mtext(expression(paste("Maize yield [", t %.% ha^{-1}, "]")), side = 2, line = 3, las = 3, at = 4)
-legend(5, 8.5, legend = c("Statistical county-level data", "Simulated for the loam profile"),
+mtext(expression(paste("Maize yield [", t %.% ha^{-1}, "]")), side = 2, line = 2.3, las = 3, at = 4)
+legend("topleft", legend = c("Statistical county-level data", "Simulated for the loam profile"),
        fill = maizecolors,  bg = "white", cex = 0.8)
+box()
+dev.off()
