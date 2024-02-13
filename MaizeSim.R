0ee6f18f8d5add9b2e525c4b47588455adc2fe53
Kalicz Péter <kaliczp@gmail.com>
2024-02-13 20:20:31 +0100

    Legend added to maize figure

--- MaizeSim.R
+++ MaizeSim.R
@@ -11,5 +11,7 @@ maizecolors <- gwcolors[2:3] # dark and lighter from FigMaizeVSGW.R
 par(mar = c(3.1,4.6,0.6,4.1), las = 2)
 barplot(Maizesim, beside = TRUE, ylim = c(0,9), col = NA)
 grid(nx=NA, ny = NULL)
-barplot(Maizesim, beside = TRUE, ylim = c(0,9), col = maizecolors)
+barplot(Maizesim, beside = TRUE, ylim = c(0,9), col = maizecolors, add = TRUE)
 mtext(expression(paste("Maize yield [", t %.% ha^{-1}, "]")), side = 2, line = 3, las = 3, at = 4)
+legend(5, 8.5, legend = c("Statistical county-level data", "Simulated for the loam profile"),
+       fill = maizecolors,  bg = "white", cex = 0.8)
